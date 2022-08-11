bb36e1281bf6014809d44638a9768f7b685e3aba
live7374 <5358207@gmail.com>
2022-08-11 23:30:46 +0300

    added onclick function + added logic to the whole game. The most interesting thing - is that all changes etc are held in odject values now

--- script.js
+++ script.js
@@ -1,25 +1,74 @@
-console.log("helloWorld")
-
 
 //function should return random value (plays on behalf of computer)
 
 //first define an array of variable
-const randomItems = ["rock", "paper", "scissors"];
+const randomItems = ["Rock", "Paper", "Scissors"];
+
+//created an object that has all values needed for game.
+let roundResult = {
+    pcPick: 'no', 
+    userPick: 'no', 
+    winner: 'no', 
+    userScore: 'no', 
+    pcScore: 'no', 
+    roundNo: 'no'}
 
 //randomly choose element from an array of variables
 function getComputerChoice()
 {
    let randomOne = randomItems[Math.floor(Math.random()*randomItems.length)];
-   return console.log(randomOne)
+   roundResult.pcPick = randomOne
+   //return console.log(randomOne)
 }
-//invoke function
-getComputerChoice();
+//invoke function. No need to invoke it from here
+//getComputerChoice();
 
-//connecting buttons from html
-const userRock = document.getElementById("userRock");
+//variable that stands for user Pick
 
-window.onload=function(){
-userRock.addEventListener('click', () =>{
-    alert('Rock Picked');
-});
+function userRock()
+{
+    roundResult.userPick = "Rock"
 };
+
+function userPaper()
+{
+    roundResult.userPick = "Paper"
+};
+
+function userScissors()
+{
+    roundResult.userPick = "Scissors"
+};
+// set logic for the game output
+
+//logic of game itself
+
+function judge()
+{
+//Rock
+if (roundResult.pcPick == "Rock" && roundResult.userPick == "Rock")
+    roundResult.winner = "Tie";
+else if (roundResult.pcPick == "Rock" && roundResult.userPick == "Scissors")
+    roundResult.winner = "PC";
+else if (roundResult.pcPick == "Rock" && roundResult.userPick == "Paper")
+    roundResult.winner = "User";
+//Paper
+else if (roundResult.pcPick == "Paper" && roundResult.userPick == "Rock")
+    roundResult.winner = "PC";
+else if (roundResult.pcPick == "Paper" && roundResult.userPick == "Scissors")
+    roundResult.winner = "User";
+else if (roundResult.pcPick == "Paper" && roundResult.userPick == "Paper")
+    roundResult.winner = "Tie";
+//Scissors
+else if (roundResult.pcPick == "Scissors" && roundResult.userPick == "Rock")
+    roundResult.winner = "User";
+else if (roundResult.pcPick == "Scissors" && roundResult.userPick == "Scissors")
+    roundResult.winner = "Tie";
+else if (roundResult.pcPick == "Scissors" && roundResult.userPick == "Paper")
+    roundResult.winner = "PC";
+/////Else
+else
+console.log("you've made an error");
+
+console.log("winner is: "+ roundResult.winner )
+}
